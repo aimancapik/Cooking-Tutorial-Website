3ff5f92cfa67a0b81a3fbe249a99379deac9d019
aiman <aimancapik@gmail.com>
2023-01-14 03:28:59 +0800

    CSS LOGIN AIMAN

--- lib/login.php
+++ lib/login.php
@@ -1,28 +1,44 @@
-<?php
-$title = "Log In";
-require_once('../layout/header_login.php');
-?>
-<script src="script/login_handler.js"></script>
-
-<form name="RegForm">
-    <div>
-        <input type="text" name="email" placeholder="Email"/>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../lib/style/aiman.css">
+    <link rel="stylesheet" href="boxicons-master/css/boxicons.css">
+    <title>Login</title>
+</head>
+<body>
+  <form name="RegForm">
+   <div class="box">
+    <div class="container">
+        <div class="top">
+            <header>Login</header>
+        </div>
+        <div class="input-field">
+            <input type="text" class="input" name="email" placeholder="Email">
+            <i class='bx bx-user' ></i>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="password" name="password" class="input" placeholder="Password">
+            <i class='bx bx-lock-alt'></i>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="button" name="send" class="submit" value="Login" onclick="return login()"/>
+        </div>
+      </form>
+        <div class="two-col">
+            <div class="one">
+               <label><a href="http://localhost/Cooking-Tutorial-Website/lib/signup.php">Sign Up</a></label>             
+            </div>
+            <div class="two">
+                <label><a href="#">Forgot password?</a></label>
+            </div>
+        </div>
     </div>
-    
-    <div>
-        <input type="password" name="password" placeholder="Password"/>
-    </div>
-    
-    <div>
-        <input type="button" name="send" value="LOG IN" onclick="return login()"/>
-    </div>
-</form>
-
-<div>
-    <a href="http://localhost/Cooking-Tutorial-Website/lib/signup.php">Sign Up</a>
-</div>
-
-
-<?php
-require_once('../layout/footer.php');
-?>
+</div> 
+<script src="script/login_handler.js"></script> 
+</body>
+</html>
--- lib/signup.php
+++ lib/signup.php
@@ -1,35 +1,54 @@
-<?php
-$title = "Sign Up";
-require_once('../layout/header_login.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../lib/style/aiman.css">
+    <link rel="stylesheet" href="boxicons-master/css/boxicons.css">
+    <title>Sign Up</title>
+</head>
+<body>
+  <form name="RegForm">
+   <div class="box">
+    <div class="container">
+        <div class="top">
+            <header>Sign Up</header>
+        </div>
+        <div class="input-field">
+            <input type="text" class="input" name="email" placeholder="Email" >
+            <i class='bx bx-user' ></i>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="text" class="input" name="name" placeholder="Username" >
+            <i class='bx bx-user' ></i>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="password" name="password" class="input" placeholder="Password" >
+            <i class='bx bx-lock-alt'></i>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="password" name="confpassword" class="input" placeholder="Confirm Password" >
+            <i class='bx bx-lock-alt'></i>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="button" name="send" class="submit" value="SIGN UP" onclick="return signup()">
+        </div>
+      </form>
+        <div class="two-col">
+            <div class="one">
+               <label><a href="http://localhost/Cooking-Tutorial-Website/lib/login.php">Sign in</a></label>             
+            </div>
+            <div class="two">
+                <label><a href="#">Forgot password?</a></label>
+            </div>
+        </div>
+    </div>
+</div>  
 <script src="script/signup_handler.js"></script>
-
-<form name="RegForm">
-    <div>
-        <input type="text" name="email" placeholder="Email"/>
-    </div>
-
-    <div>
-        <input type="text" name="name" placeholder="Name"/>
-    </div>
-    
-    <div>
-        <input type="password" name="password" placeholder="Password"/>
-    </div>
-
-    <div>
-        <input type="password" name="confpassword" placeholder="Confirm Password"/>
-    </div>
-    
-    <div>
-        <input type="button" name="send" value="SIGN UP" onclick="return signup()"/>
-    </div>
-</form>
-
-<div>
-    <a href="http://localhost/Cooking-Tutorial-Website/lib/login.php">Log In</a>
-</div>
-
-<?php
-require_once('../layout/footer.php');
-?>
+</body>
+</html>
